a103732398908bccb09180d828302a01bca856a7
devanksinghchaudhary <devanksinghchaudhary@gmail.com>
2026-08-02 10:06:45 +0530

    #002: added other .geojson

--- src/vcp/globe/main.py
+++ src/vcp/globe/main.py
@@ -1,14 +1,21 @@
 import geopandas as gpd
 from vcp.utils import root
 
-DATASET = root.find() / "src" / "dataset" / "globe" / "packages" / "natural_earth_vector.gpkg"
+DATASET = root.find() / "src" / "vcp" / "globe" / "packages" / "natural_earth_vector.gpkg"
+EXTRACTED = root.find() / "src" / "vcp" / "globe" / "extracted"
+
+layers = gpd.list_layers(DATASET)
+for layer in layers["name"]:
+    print(layer)
+
+input = input("Enter File to Extract: ")
 
 land = gpd.read_file(
     DATASET,
-    layer="ne_110m_land"
+    layer=input
 )
 
 land.to_file(
-    "land.geojson",
-    driver="GeoJSON"
+        f"{EXTRACTED}/{input}.geojson",
+        driver="GeoJSON"
 )
